c223ccf45fb11883af3aa6913a4663ae997a2515
Abdelrahman Salah <abdelrahman1809095@miuegypt.edu.eg>
2021-11-18 23:54:42 +0200

    login

---
+++ login.dart
@@ -0,0 +1,30 @@
+
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Login(),
+    );
+  }
+}
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+    appBar:AppBar(
+    
+
+    ) ,
+
+       );
+  }
+}
