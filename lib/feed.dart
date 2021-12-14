b0070ff3f433feb637fd5976414ec06747ec2f73
shams-abdelhamid <63125459+shams-abdelhamid@users.noreply.github.com>
2021-12-14 23:21:04 +0200

    feed

---
+++ lib/feed.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:loginsignup/imorts.dart';
+
+class Feed extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawerwidget(),
+      appBar: Appbarwidget('Feed'),
+      bottomSheet: Footerwidget(),
+      body: Column(
+        children: [Post('Egycon')],
+      ),
+    );
+  }
+}
+
+/*class Post extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return ;
+  }
+}*/
