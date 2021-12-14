ded31c6ef2614204832eaa9f0ec3d7a6a2bdbfd1
Abdelrahman Salah <abdelrahman1809095@miuegypt.edu.eg>
2021-12-14 23:11:19 +0200

    profile update

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -20,36 +20,52 @@ class Profile extends StatelessWidget {
     return Scaffold(
         drawer: Drawerwidget(),
         appBar: Appbarwidget('profile'),
-        body: ListView(
-          padding:
-              EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.25),
-          children: [
-            Row(
-              children: [
-                Text(
-                  "Club Full Slogan",
-                  style: TextStyle(
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.red),
-                ),
-                IconButton(
-                    padding: EdgeInsets.only(
-                        left: MediaQuery.of(context).size.width * 0.15),
-                    onPressed: () {},
-                    icon: Icon(Icons.settings))
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                  left: MediaQuery.of(context).size.width * 0.15),
-              child: Text(
-                "Board",
-                style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+        body: Center(
+             
+              child: Column(
+                
+                children: [
+                  
+                     Padding(
+                       padding: EdgeInsets.only(left:MediaQuery.of(context).size.width*0.25),
+                       child: Row(
+                        
+                        children: [
+                         
+                             Text(
+                              "Club Full Slogan",
+                              style: TextStyle(
+                                  fontSize: 24.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.red),
+                            ),
+                          
+                          IconButton(
+                               padding: EdgeInsets.only(left:MediaQuery.of(context).size.width*0.15),
+                              onPressed: () {},
+                              icon: Icon(Icons.settings))
+                        ],
+                    ),
+                     ),
+                  
+                  
+                    Text(
+                      "Board",
+                      style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                    ),
+                  
+                  Padding(
+                    padding: const EdgeInsets.only(top:10.0),
+                    child: Container(child: Image.asset('assets/images/owner.png')),
+                  ),
+                   Padding(
+                    padding: const EdgeInsets.only(top:35.0),
+                    child: Container(child: Image.asset('assets/images/president.png')),
+                  ),
+                ],
               ),
             ),
-            Container(child: Image.asset('assets/images/owner.png')),
-          ],
-        ));
+          
+        );
   }
 }
