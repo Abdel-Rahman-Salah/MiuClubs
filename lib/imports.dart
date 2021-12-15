dfbc3ec3508db71c09f4fc2acc156e68af1b065f
Abdelrahman Salah <abdelrahman1809095@miuegypt.edu.eg>
2021-12-15 16:57:20 +0200

    Imports update

--- lib/imports.dart
+++ lib/imports.dart
@@ -49,7 +49,7 @@ class Appbarwidget extends StatelessWidget with PreferredSizeWidget {
       actions: [Icon(Icons.logout)],
       bottom: PreferredSize(
           child:
-              Text(page!, style: TextStyle(fontSize: 30, color: Colors.white)),
+              Text(page!, style: TextStyle(fontSize: 30, color:Color. fromRGBO(254, 3, 3, 1.0))),
           preferredSize: Size(40, 40)),
       centerTitle: true,
       backgroundColor: Color.fromRGBO(51, 51, 51, 1.0),
