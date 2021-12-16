cf957c776d90b5fca5a5ed91a298cde81b1d2e8d
zeina <zeina.ayman@hotmail.com>
2021-12-16 11:56:16 +0200

    added navigation and adminhomepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:loginsignup/feed.dart';
+import 'package:loginsignup/screens/manage_Clubs.dart';
 import 'package:loginsignup/signin/signin.dart';
 import 'package:loginsignup/imports.dart';
-import 'package:loginsignup/view/splash_screen.dart';
+import 'package:loginsignup/screens/splash_screen.dart';
+
+import 'screens/add_Club.dart';
+import 'screens/admin_homepage.dart';
 
 var routes = <String, WidgetBuilder>{
   "/login": (BuildContext context) => Signin(),
+  "/admin": (BuildContext context) => AdminHomePage(),
+  "/create": (BuildContext context) => addClub(),
+  "/manage": (BuildContext context) => ManageClubs(),
 };
 
 void main() => runApp(MaterialApp(
     theme:
         ThemeData(primaryColor: Colors.red, accentColor: Colors.yellowAccent),
     debugShowCheckedModeBanner: false,
-    home: Feed(),
+    home: SplashScreen(),
     routes: routes));
--- lib/navigator.dart
+++ lib/navigator.dart
@@ -4,4 +4,16 @@ class MyNavigator {
   static void GoLogin(BuildContext context) {
     Navigator.pushNamed(context, "/login");
   }
+
+  static void goToAdminHomepage(BuildContext context) {
+    Navigator.pushNamed(context, "/admin");
+  }
+
+  static void goToAddClub(BuildContext context) {
+    Navigator.pushNamed(context, "/create");
+  }
+
+  static void goToManageClubs(BuildContext context) {
+    Navigator.pushNamed(context, "/manage");
+  }
 }
