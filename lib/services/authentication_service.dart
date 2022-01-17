0a4fad8dde1858c11ce5776fae2ae7920f75be24
Ahmed Hany <76770296+TheHanys@users.noreply.github.com>
2022-01-17 17:43:45 +0200

    Signup added

--- lib/services/authentication_service.dart
+++ lib/services/authentication_service.dart
@@ -19,3 +19,25 @@ Future<String> Signin({required String email, required String password}) async {
 	}
 	}
 }
+
+Future<String> SignUp({String email, String password}) async {
+	try {
+	await _firebaseAuth.createUserWithEmailAndPassword(
+		email: email, password: password);
+	return "Signed Up";
+	} on FirebaseAuthException catch (e) {
+	if (e.code == 'weak-password') {
+		print(e.message);
+		return e.message;
+	} else if (e.code == 'email-already-in-use') {
+		print(e.message);
+		return e.message;
+	} else {
+		print(e.message);
+		return e.message;
+	}
+	} catch (e) {
+	print(e);
+	}
+}
+
