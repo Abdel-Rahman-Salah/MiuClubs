8b70cb2f02b83fb718a33e4ba8ab1e98e75f4b3a
Abdelrahman Salah <abdelrahman1809095@miuegypt.edu.eg>
2022-01-21 20:13:53 +0200

    adding clubs services

--- lib/services/fire_store_services.dart
+++ lib/services/fire_store_services.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+
 class FireStoreServicesx {
   static var uid = FirebaseAuth.instance.currentUser!.uid;
   var posts = FirebaseFirestore.instance.collection('Posts');
@@ -11,5 +12,18 @@ class FireStoreServicesx {
     try {
       log(posts.doc(uid).collection('Posts').toString());
     } catch (e) {}
+  }
+ 
+      CollectionReference clubsCollection =
+      FirebaseFirestore.instance.collection('Clubs');
+      
+      Future<List> getClubs() async {
+      QuerySnapshot querySnapshot = await clubsCollection.get();
+    
+    
+      final data = querySnapshot.docs.map((doc) => doc.data()).toList();
+    
+      log('$data') ;
+      return data;
   }
 }
