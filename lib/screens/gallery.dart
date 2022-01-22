45325979804ca4e545082b5f68e245317bcdd2fb
Abdelrahman Salah <abdelrahman1809095@miuegypt.edu.eg>
2022-01-22 20:00:25 +0200

    moving files

--- lib/screens/gallery.dart
+++ lib/screens/gallery.dart
@@ -1,35 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:loginsignup/layout/constant.dart';
 import 'package:loginsignup/layout/imports.dart';
 
 class Gallery extends StatelessWidget {
+  const Gallery({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawerwidget(),
-      appBar: Appbarwidget('Gallery'),
-      bottomSheet: Footerwidget(),
-      body: Column(
+    var container = Container(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Row(children:[
-            Image.asset('images/lake.jpg'),
-            Image.asset('images/lake.jpg'),
-            Image.asset('images/lake.jpg'),
-          ],),
-
-           Row(children:[
-            Image.asset('images/lake.jpg'),
-            Image.asset('images/lake.jpg'),
-            Image.asset('images/lake.jpg'),
-          ],),
-
-           Row(children:[
-            Image.asset('images/lake.jpg'),
-            Image.asset('images/lake.jpg'),
-            Image.asset('images/lake.jpg'),
-          ],),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Flexible(
+                child: Image.asset(
+                  'assets/images/e.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Flexible(
+                child: Image.asset(
+                  'assets/images/e.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Flexible(
+                child: Image.asset(
+                  'assets/images/e.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Flexible(
+                child: Image.asset(
+                  'assets/images/mic.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Flexible(
+                child: Image.asset(
+                  'assets/images/e.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Flexible(
+                child: Image.asset(
+                  'assets/images/e.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Flexible(
+                child: Image.asset(
+                  'assets/images/e.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Flexible(
+                child: Image.asset(
+                  'assets/images/e.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Flexible(
+                child: Image.asset(
+                  'assets/images/e.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Gallery'),
+        ),
+        body: container);
   }
 }
