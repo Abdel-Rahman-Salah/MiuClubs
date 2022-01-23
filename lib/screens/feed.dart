536e4a8a9bb24ed6102af6f5bbdd3cd6df118d12
shams-abdelhamid <63125459+shams-abdelhamid@users.noreply.github.com>
2022-01-23 19:02:06 +0200

    pushnotes

--- lib/screens/feed.dart
+++ lib/screens/feed.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 import 'dart:ffi';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:loginsignup/layout/imports.dart';
@@ -30,14 +31,32 @@ class _FeedState extends State<Feed> {
   void initState() {
     super.initState();
     FirebaseMessaging.instance.getInitialMessage();
-    Future<void> firebaseMessagingBackgroundHandler(
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Got a message whilst in the foreground!');
+      print('Message data: ${message.data}');
+
+      if (message.notification != null) {
+        print('Message also contained a notification: ${message.notification}');
+      }
+    });
+
+    Future<void> _firebaseMessagingBackgroundHandler(
         RemoteMessage message) async {
-      print("Handling a background message");
+      await Firebase.initializeApp();
+      print('Handling a background message ${message.messageId}');
     }
 
-    FirebaseMessaging.onMessage.listen(firebaseMessagingBackgroundHandler);
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
-    _firebasemessaging.requestPermission(sound: true, badge: true, alert: true);
+    Future<NotificationSettings> reqperms() async {
+      NotificationSettings settings = await _firebasemessaging
+          .requestPermission(sound: true, badge: true, alert: true);
+      log('permisiion: ${settings.authorizationStatus}');
+      return settings;
+    }
+
+    Future<NotificationSettings> settings = reqperms();
   }
 
   @override
