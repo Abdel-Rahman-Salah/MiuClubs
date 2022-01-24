2eda4bd52dbfb87b818408eb43ed3223ff9b1ac9
NayraMawardyy <96235686+NayraMawardyy@users.noreply.github.com>
2022-01-24 10:57:19 +0200

    Update gallery.dart

--- lib/screens/gallery.dart
+++ lib/screens/gallery.dart
@@ -1,92 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:loginsignup/layout/constant.dart';
-import 'package:loginsignup/layout/imports.dart';
+import 'package:nayraa/gallery.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class Gallery extends StatelessWidget {
   const Gallery({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    var container = Container(
+    /* var container = Container(
       child: Column(
         mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Flexible(
-                child: Image.asset(
-                  'assets/images/e.jpg',
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Flexible(
-                child: Image.asset(
-                  'assets/images/e.jpg',
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Flexible(
-                child: Image.asset(
-                  'assets/images/e.jpg',
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Flexible(
-                child: Image.asset(
-                  'assets/images/mic.jpg',
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Flexible(
-                child: Image.asset(
-                  'assets/images/e.jpg',
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Flexible(
-                child: Image.asset(
-                  'assets/images/e.jpg',
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Flexible(
-                child: Image.asset(
-                  'assets/images/e.jpg',
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Flexible(
-                child: Image.asset(
-                  'assets/images/e.jpg',
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Flexible(
-                child: Image.asset(
-                  'assets/images/e.jpg',
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
+          Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+            IconButton(
+                onPressed: () {
+                  // MyNavigator.goToAddClub(context);
+                },
+                icon: Icon(
+                  Icons.edit,
+                  color: Colors.grey[700],
+                ))
+          ]), */
+    // Wrap(alignment: ,)
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Gallery'),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(50.0),
+        child: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'MIU CLUBS',
+            style: TextStyle(color: Colors.red),
+          ),
+          backgroundColor: Colors.black,
         ),
-        body: container);
+      ),
+      body: Container(
+          child: GridView.builder(
+        padding: EdgeInsets.all(12.0),
+        itemCount: 9,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3, crossAxisSpacing: 4.0, mainAxisSpacing: 6.0),
+        itemBuilder: (BuildContext context, int index) {
+          return Image.asset(
+            'assets/images/e.jpg',
+            fit: BoxFit.cover,
+          );
+        },
+      )
+          //]
+          //)
+          //])
+          //])
+          ),
+    );
   }
 }
